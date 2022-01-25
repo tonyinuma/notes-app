b08e31787064f58573e8d81c8129aa85f8ac2313
Tony ID <tony.inuma@gmail.com>
2022-01-24 21:50:20 -0500

    Add: Auth Router styles in login and register screen

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,7 +1,13 @@
 export const LoginScreen = () => {
     return (
-        <div>
-            LoginScreen
-        </div>
+        <>
+            <h3>Login</h3>
+            <form>
+                <input type="text" placeholder="email" name="email"/>
+                <input type="password" placeholder="****" name="password"/>
+                <button type="submit">Login</button>
+            </form>
+            google
+        </>
     );
 };
--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -4,10 +4,14 @@ import {RegisterScreen} from "../components/auth/RegisterScreen";
 
 export const AuthRouter = () => {
     return (
-        <Switch>
-            <Route path="/auth/login" component={LoginScreen} exact/>
-            <Route path="/auth/register" component={RegisterScreen} exact/>
-            <Redirect to="/auth/login"/>
-        </Switch>
+        <div className="auth__main">
+            <div className="auth__box-container">
+                <Switch>
+                    <Route path="/auth/login" component={LoginScreen} exact/>
+                    <Route path="/auth/register" component={RegisterScreen} exact/>
+                    <Redirect to="/auth/login"/>
+                </Switch>
+            </div>
+        </div>
     );
 };
