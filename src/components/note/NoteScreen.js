cca39808df08464b7735dff92dad0d84f6c530db
Tony ID <tony.inuma@gmail.com>
2022-01-24 23:12:47 -0500

    Feat: Sidebar component design

--- src/components/note/NoteScreen.js
+++ src/components/note/NoteScreen.js
@@ -1,7 +1,12 @@
+import {Sidebar} from "./Sidebar";
+
 export const NoteScreen = () => {
     return (
-        <div>
-            NoteScreen
+        <div className="note__main-content">
+            <Sidebar/>
+            <main>
+                <h1>Content</h1>
+            </main>
         </div>
     );
 };
--- src/components/note/Sidebar.js
+++ src/components/note/Sidebar.js
@@ -1,7 +1,19 @@
 export const Sidebar = () => {
     return (
-        <div>
-            Sidebar
-        </div>
+        <aside className="note__sidebar">
+            <div className="note_sidebar-navbar">
+                <h3>
+                    <i className="far fa-moon"/>
+                    <span> Tony</span>
+                </h3>
+
+                <button className="btn">Logout</button>
+            </div>
+            <div className="note__new-entry">
+                <i className="far fa-calendar-plus fa-5x"/>
+                <p className="mt-5">New Entry</p>
+            </div>
+
+        </aside>
     );
 };
