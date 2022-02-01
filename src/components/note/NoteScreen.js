9df2a18a1b25b4f0cc0fecc631260e2774059c1b
Tony ID <tony.inuma@gmail.com>
2022-01-31 22:34:20 -0500

    Add: Nothing Selected styles

--- src/components/note/NoteScreen.js
+++ src/components/note/NoteScreen.js
@@ -1,12 +1,12 @@
 import {Sidebar} from "./Sidebar";
-import {NothingSelected} from "./NothingSelected";
+// import {NothingSelected} from "./NothingSelected";
 
 export const NoteScreen = () => {
     return (
         <div className="note__main-content">
             <Sidebar/>
             <main>
-                <NothingSelected/>
+                {/*<NothingSelected/>*/}
             </main>
         </div>
     );
