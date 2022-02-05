d759100ee22c9ac0663c42cbdeea50524c0554c1
Tony ID <tony.inuma@gmail.com>
2022-02-04 22:38:52 -0500

    Add: Sheet Screen component form

--- src/components/sheets/SheetScreen.js
+++ src/components/sheets/SheetScreen.js
@@ -4,6 +4,20 @@ export const SheetScreen = () => {
     return (
         <div className="sheets__main-content">
             <SheetsAppBar/>
+
+            <div className="sheets__content">
+                <input type="text"
+                       className="sheets__title_input"
+                       placeholder="Some Awesome Title"
+                       autoComplete="off"/>
+                <textarea className="sheets__textarea"
+                          placeholder="What happened today?">
+                </textarea>
+                <div className="sheets__image">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMeXZAympIZl23wKL9n7BfWfxbxtr7UnM8xw&usqp=CAU"
+                         alt="imagen"/>
+                </div>
+            </div>
         </div>
     );
 };
