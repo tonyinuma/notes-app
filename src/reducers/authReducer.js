e9d88393f76bba655b8ac06fecdfc5745e43184e
Tony ID <tony.inuma@gmail.com>
2022-02-06 21:56:05 -0500

    Fix: uid variable in reducer is inside payload

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -4,7 +4,7 @@ export const authReducer = (state = {}, action) => {
     switch (action.type) {
         case types.login:
             return {
-                uid: action.uid,
+                uid: action.payload.uid,
                 name: action.payload.displayName,
             }
         case types.logout:
