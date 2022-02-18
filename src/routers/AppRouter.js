89ad5e1692b3544baf8f734fe2d44c5f483f80c3
Tony ID <tony.inuma@gmail.com>
2022-02-17 22:57:17 -0500

    Feat: Global Loader

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 import {NoteScreen} from "../components/note/NoteScreen";
 import {AuthRouter} from "./AuthRouter";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {auth, onAuthStateChanged} from "../firebase/firebaseConfig";
 import {useDispatch} from "react-redux";
 import {login} from "../actions/auth";
@@ -9,14 +9,24 @@ import {login} from "../actions/auth";
 export const AppRouter = () => {
 
     const dispatch = useDispatch();
+    const [checking, setChecking] = useState(true);
+    const [authenticated, setAuthenticated] = useState(false);
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             if (user?.uid) {
                 dispatch(login(user.uid, user.displayName));
+                setAuthenticated(true);
+            } else {
+                setAuthenticated(false);
             }
+            setChecking(false);
         });
     }, [dispatch]);
+
+    if (checking) {
+        return <h1>Wait a moment...</h1>;
+    }
 
     return (
         <Router>
