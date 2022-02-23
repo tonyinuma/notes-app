270163fbe06cb5a5e3793f3f83cd3569564aa018
Tony ID <tony.inuma@gmail.com>
2022-02-23 09:58:13 -0500

    Feat: use sheet state to show or not sheet screen using active property

--- src/components/note/NoteScreen.js
+++ src/components/note/NoteScreen.js
@@ -1,14 +1,17 @@
 import {Sidebar} from "./Sidebar";
 import {SheetScreen} from "../sheets/SheetScreen";
-// import {NothingSelected} from "./NothingSelected";
+import {useSelector} from "react-redux";
+import {NothingSelected} from "./NothingSelected";
 
 export const NoteScreen = () => {
+
+    const {active} = useSelector(state => state.sheet);
+
     return (
         <div className="note__main-content">
             <Sidebar/>
             <main>
-                {/*<NothingSelected/>*/}
-                <SheetScreen/>
+                {active ? <SheetScreen/> : <NothingSelected/>}
             </main>
         </div>
     );
