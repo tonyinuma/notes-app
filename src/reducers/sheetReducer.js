5950c2630d2174248e85356f01b3158daea341ff
Tony ID <tony.inuma@gmail.com>
2022-02-23 09:57:21 -0500

    Feat: new Sheet Reducer and register in store

---
+++ src/reducers/sheetReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    sheets: [],
+    active: null
+}
+
+export const sheetReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,15 @@
 import {applyMiddleware, combineReducers, compose, createStore} from "redux";
+import thunk from "redux-thunk";
 import {authReducer} from "../reducers/authReducer";
 import {uiReducer} from "../reducers/uiReducer";
-import thunk from "redux-thunk";
+import {sheetReducer} from "../reducers/sheetReducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const reducers = combineReducers({
     auth: authReducer,
-    ui: uiReducer
+    ui: uiReducer,
+    sheet: sheetReducer
 });
 
 export const store = createStore(
