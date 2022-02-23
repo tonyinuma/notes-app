8a157fb316671e9ca9effc7dbbee8877e852f235
Tony ID <tony.inuma@gmail.com>
2022-02-23 11:04:05 -0500

    Add: Types to use in Sheets

--- src/types/types.js
+++ src/types/types.js
@@ -1,8 +1,17 @@
 export const types = {
     login: '[Auth] Login',
     logout: '[Auth] Logout',
+
     uiSetError: '[UI] Set Error',
     uiRemoveError: '[UI] Remove Error',
     uiStartLoading: '[UI] Start Loading',
-    uiFinishLoading: '[UI] Finish Loading'
+    uiFinishLoading: '[UI] Finish Loading',
+
+    sheetNew: '[Sheet] Add New Sheet',
+    sheetActive: '[Sheet] Set Active Sheet',
+    sheetLoad: '[Sheet] Load Sheets',
+    sheetUpdate: '[Sheet] Update Sheet',
+    sheetUpdateFile: '[Sheet] Update File Url Sheet',
+    sheetDelete: '[Sheet] Delete Sheet',
+    sheetCleanLogout: '[Sheet] Clean Logout',
 }
